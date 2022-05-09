e60585d80aac5bd44f240a35308c1b52420ad2f4
toanbanguyennn <toan05111999@gmail.com>
2022-05-09 09:24:48 +0700

    S-P hih tron

---
+++ dientich-chuvihinhtron.js
@@ -0,0 +1,9 @@
+inputr= prompt("bán kính hình tròn")
+const PI = 3.14159
+let r = parseInt(inputr)
+let S = PI*r*r
+let P = PI*2*r
+document.write("Dientich : " +S );
+document.write("<br/>");
+
+document.write("Chuvi : " +P );
